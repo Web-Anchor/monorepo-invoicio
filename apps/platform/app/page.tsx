8d3c6254e67202498eff41bc00644421cde3e89b
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-07-03 15:21:08 +0100

    set up platform app & monorepo structure

--- apps/platform/app/page.tsx
+++ apps/platform/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className="text-4xl font-bold">Welcome to the Portal</p>
+      <p className="text-4xl font-bold">Welcome to the Platform</p>
     </main>
   );
 }
